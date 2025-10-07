338690c250e8936ab7a1aaf0d15976a66ba6cd96
sante <santersraels0@gmail.com>
2025-10-07 12:34:19 +0100

    Change Profile app port.

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ import defaultErrHandler from "./errHandler/index.js";
 import "./db/index.js";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
